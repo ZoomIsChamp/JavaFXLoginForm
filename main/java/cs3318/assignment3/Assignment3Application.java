0effe9f696bb66535144cd90b9ac55cc48fd42a4
Odhran Sexton <92513044+ZoomIsChamp@users.noreply.github.com>
2021-12-01 14:41:44 +0000

    Transferred tests to User

--- main/java/cs3318/assignment3/Assignment3Application.java
+++ main/java/cs3318/assignment3/Assignment3Application.java
@@ -6,58 +6,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class Assignment3Application extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("Registration Form");
-        FXMLLoader registerFXML = new FXMLLoader(Assignment3.class.getResource("Assignment3.fxml"));
+        FXMLLoader registerFXML = new FXMLLoader(User.class.getResource("Assignment3.fxml"));
         Scene scene = new Scene(registerFXML.load(), 300, 275);
+
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch();
-    }
-
-
-    public boolean checkEmail(String testEmail) {
-        final Pattern VALID_EMAIL_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = VALID_EMAIL_REGEX.matcher(testEmail);
-        if (!matcher.find()) {
-            throw new IllegalArgumentException("Invalid Email Address");
-        }
-        return true;
-    }
-
-    public boolean checkPasswordLength(String testPassword) {
-        return testPassword.length() >= 7;
-    }
-
-    public boolean passwordContainsSpecial(String testPassword) {
-        final Pattern PASSWORD_CONTAINS = Pattern.compile("[*^&@!]+");
-        Matcher matcher = PASSWORD_CONTAINS.matcher(testPassword);
-        return matcher.find();
-    }
-
-    public boolean passwordContainsNumber(String testPassword) {
-        final Pattern PASSWORD_CONTAINS_NUMBER = Pattern.compile("\\d");
-        Matcher matcher = PASSWORD_CONTAINS_NUMBER.matcher(testPassword);
-        return matcher.find();
-    }
-
-    public boolean passwordContainsLetter(String testPassword) {
-        final Pattern PASSWORD_CONTAINS_LETTER = Pattern.compile("[A-Z]", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = PASSWORD_CONTAINS_LETTER.matcher(testPassword);
-        return matcher.find();
-    }
-
-
-    public boolean isValidPassword(String testPassword) {
-        return (checkPasswordLength(testPassword) && passwordContainsSpecial(testPassword) && passwordContainsNumber(testPassword) && passwordContainsLetter(testPassword));
     }
 }
