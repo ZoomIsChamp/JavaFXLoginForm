3d37a0a7566c94f7519d34b99743558a4bdc115c
Odhran Sexton <92513044+ZoomIsChamp@users.noreply.github.com>
2021-12-01 11:24:28 +0000

    Added test to reject Invalid emails

--- main/test/cs3318/assignment3/Assignment3Test.java
+++ main/test/cs3318/assignment3/Assignment3Test.java
@@ -2,5 +2,11 @@ package cs3318.assignment3;
 
 public class Assignment3Test {
     final static Assignment3 tester = new Assignment3();
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"", "@gmail.com", "abc@.com", "fake email@address.com"})
+    public void rejectsInvalidEmail(String testEmail) {
+        assertThrows(IllegalArgumentException.class, () -> { tester.checkEmail(testEmail); });
+    }
 
 }
