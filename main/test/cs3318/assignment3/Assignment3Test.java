b778539be981d743af260b0d7a76eafd627fa0e8
Odhran Sexton <92513044+ZoomIsChamp@users.noreply.github.com>
2021-12-01 11:27:00 +0000

    Added test to accept Valid Emails

--- main/test/cs3318/assignment3/Assignment3Test.java
+++ main/test/cs3318/assignment3/Assignment3Test.java
@@ -1,5 +1,11 @@
 package cs3318.assignment3;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class Assignment3Test {
     final static Assignment3 tester = new Assignment3();
     
@@ -7,6 +13,12 @@ public class Assignment3Test {
     @ValueSource(strings = {"", "@gmail.com", "abc@.com", "fake email@address.com"})
     public void rejectsInvalidEmail(String testEmail) {
         assertThrows(IllegalArgumentException.class, () -> { tester.checkEmail(testEmail); });
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"abc@gmail.com", "xyz@123.com", "real@address.co.uk", "real.email@address.ie"})
+    public void acceptsValidEmail(String testEmail) {
+        assertTrue(tester.checkEmail(testEmail));
     }
 
 }
