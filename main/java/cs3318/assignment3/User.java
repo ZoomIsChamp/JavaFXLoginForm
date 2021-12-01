6e2ce63a3b1ed2a56cf170b0bd7cb63c30f75bc2
Odhran Sexton <92513044+ZoomIsChamp@users.noreply.github.com>
2021-12-01 14:39:17 +0000

    Created a User with basic functions

---
+++ main/java/cs3318/assignment3/User.java
@@ -0,0 +1,18 @@
+package cs3318.assignment3;
+
+public class User {
+    private final String email;
+    private final String password;
+
+    public User (String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
